b79b1de86266f107e1fbb024af94cde018d32d4f
melo <melih@DESKTOP-MRA5AHM>
2026-06-12 21:30:36 +0300

    Update SEO domain to abrasmarttech.com

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://abratekmer.vercel.app/sitemap.xml',
+    sitemap: 'https://www.abrasmarttech.com/sitemap.xml',
   }
 }
